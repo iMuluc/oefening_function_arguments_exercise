a57ff64481ceb4993d80efe36be4242cab3f56fc
iMuluc <iMertUluc@gmail.com>
2021-08-13 16:43:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const paint = function (direction, color) {
+    console.log("The " + direction + " wall has been painted " + color)
+};
+
+paint("", "red.")
+paint("", "green.")
+paint()
+
+paint("south-east", "grey.")
